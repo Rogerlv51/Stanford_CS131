a3b1c26b57d473ec25466be60595846aa2b23329
Rogerlv51 <949608370@qq.com>
2024-06-19 19:42:28 +0800

    Feature: Canny算子和Hough变换作业实现

--- CS131_release/fall_2020/hw2_release/edge.py
+++ CS131_release/fall_2020/hw2_release/edge.py
@@ -27,16 +27,21 @@ def conv(image, kernel):
     Hk, Wk = kernel.shape
     out = np.zeros((Hi, Wi))
 
+    kernel = np.flip(kernel, 0)
+    kernel = np.flip(kernel, 1)
     # For this assignment, we will use edge values to pad the images.
     # Zero padding will make derivatives at the image boundary very big,
     # whereas we want to ignore the edges at the boundary.
     pad_width0 = Hk // 2
     pad_width1 = Wk // 2
     pad_width = ((pad_width0,pad_width0),(pad_width1,pad_width1))
+    # 这里为了过渡自然，所以直接用图片边缘填充，没有直接用0填充
     padded = np.pad(image, pad_width, mode='edge')
 
     ### YOUR CODE HERE
-    pass
+    for i in range(Hi):
+        for j in range(Wi):
+            out[i, j] = np.sum(padded[i:i+Hk, j:j+Wk]*kernel)
     ### END YOUR CODE
 
     return out
@@ -61,9 +66,12 @@ def gaussian_kernel(size, sigma):
     kernel = np.zeros((size, size))
 
     ### YOUR CODE HERE
-    pass
+    k = (size-1) // 2
+    for i in range(size):
+        for j in range(size):
+            kernel[i, j] = (np.exp(((i-k)**2+(j-k)**2)/(-2*sigma**2)))/(2*np.pi*sigma**2)
     ### END YOUR CODE
-
+    # print(kernel)  # 打印一下看看答案有没有出入
     return kernel
 
 def partial_x(img):
@@ -81,7 +89,9 @@ def partial_x(img):
     out = None
 
     ### YOUR CODE HERE
-    pass
+    # 这边直接按照我们前面学习过的矩阵卷积即可，来求导，只需要注意x方向和y方向的向量方向即可
+    x_kernel = np.array([[0.5, 0.0, -0.5]])
+    out = conv(img, x_kernel)
     ### END YOUR CODE
 
     return out
@@ -101,7 +111,8 @@ def partial_y(img):
     out = None
 
     ### YOUR CODE HERE
-    pass
+    y_kernel = np.array([[0.5], [0.0], [-0.5]])
+    out = conv(img, y_kernel)
     ### END YOUR CODE
 
     return out
@@ -125,9 +136,11 @@ def gradient(img):
     theta = np.zeros(img.shape)
 
     ### YOUR CODE HERE
-    pass
+    G = np.sqrt(partial_x(img)**2+partial_y(img)**2)
+    # 注意弧度转角度
+    theta = np.arctan2(partial_y(img), partial_x(img))/np.pi*180
+    theta[theta < 0] += 360
     ### END YOUR CODE
-
     return G, theta
 
 
@@ -149,10 +162,49 @@ def non_maximum_suppression(G, theta):
 
     # Round the gradient direction to the nearest 45 degrees
     theta = np.floor((theta + 22.5) / 45) * 45
+    theta = theta%360   # 360°等价于0°
+    # print(theta)
 
     #print(G)
     ### BEGIN YOUR CODE
-    pass
+    # 为了搜索当前像素点的八邻域，所以先进行padding
+    
+    pad_width = ((1, 1), (1, 1))
+    G_pad = np.pad(G, pad_width=pad_width, mode='constant', constant_values=0)
+    # 接下来只需要给局部最大值赋值即可, 需要注意的是搜索的区域是严当前梯度方向的两个法线方向
+    for i in range(1, H+1):
+        for j in range(1, W+1):
+            # 由于之前的theta已经根据45°取整，所以下面只需要找当前梯度正负两个方向即可
+            if theta[i-1][j-1] == 45 or theta[i-1][j-1] == 225:
+                if G_pad[i][j] >= G_pad[i-1][j-1] and G_pad[i][j] >= G_pad[i+1][j+1]:
+                    out[i-1][j-1] = G_pad[i][j]
+            if theta[i-1][j-1] == 90 or theta[i-1][j-1] == 270:
+                if G_pad[i][j] >= G_pad[i-1][j] and G_pad[i][j] >= G_pad[i+1][j]:
+                    out[i-1][j-1] = G_pad[i][j]
+            if theta[i-1][j-1] == 135 or theta[i-1][j-1] == 315:
+                if G_pad[i][j] >= G_pad[i-1][j+1] and G_pad[i][j] >= G_pad[i+1][j-1]:
+                    out[i-1][j-1] = G_pad[i][j]
+            if theta[i-1][j-1] == 0 or theta[i-1][j-1] == 180:
+                if G_pad[i][j] >= G_pad[i][j-1] and G_pad[i][j] >= G_pad[i][j+1]:
+                    out[i-1][j-1] = G_pad[i][j]
+    '''
+    # 实际上这两种方法得到的效果在这个测试图上没什么区别，只是一个排除边界一个没有而已
+    for i in range(1, H-1):
+        for j in range(1,W-1):
+            current_angle = theta[i,j]
+            if current_angle == 0 or current_angle == 180:
+                neighbors = [G[i, j-1], G[i, j+1]]
+            elif current_angle == 45 or current_angle == 225:
+                neighbors = [G[i-1, j-1], G[i+1, j+1]]
+            elif current_angle == 90 or current_angle == 270:
+                neighbors = [G[i-1, j], G[i+1, j]]
+            elif current_angle == 135 or current_angle == 315:
+                neighbors = [G[i-1, j+1], G[i+1, j-1]]
+            else:
+                raise RuntimeError("Wrong theta value {}- should be one of the following[0,45,90,135,180,225,270,315]".format(current_angle))
+            if G[i,j] >= np.max(neighbors):
+                out[i,j] = G[i,j]
+    '''
     ### END YOUR CODE
 
     return out
@@ -177,7 +229,8 @@ def double_thresholding(img, high, low):
     weak_edges = np.zeros(img.shape, dtype=np.bool)
 
     ### YOUR CODE HERE
-    pass
+    strong_edges =  img > high
+    weak_edges = (img <= high) & (img > low)
     ### END YOUR CODE
 
     return strong_edges, weak_edges
@@ -236,7 +289,16 @@ def link_edges(strong_edges, weak_edges):
     edges = np.copy(strong_edges)
 
     ### YOUR CODE HERE
-    pass
+    while len(indices) != 0:
+        y,x = indices[0]
+        indices = np.delete(indices, 0, axis=0)
+        neighbors = get_neighbors(y, x, H, W)
+        for m,n in neighbors:
+            if weak_edges[m,n]:
+                if not edges[m,n] == True:
+                    edges[m,n] = True
+                    indices = np.vstack((indices, (m,n)))
+
     ### END YOUR CODE
 
     return edges
@@ -254,7 +316,12 @@ def canny(img, kernel_size=5, sigma=1.4, high=20, low=15):
         edge: numpy array of shape(H, W).
     """
     ### YOUR CODE HERE
-    pass
+    kernel = gaussian_kernel(kernel_size, sigma)
+    img_smooth = conv(img, kernel)
+    G, theta = gradient(img_smooth)
+    img_suppres = non_maximum_suppression(G, theta)
+    strong_edges, weak_edges = double_thresholding(img_suppres, high, low)
+    edge = link_edges(strong_edges, weak_edges)
     ### END YOUR CODE
 
     return edge
@@ -277,8 +344,8 @@ def hough_transform(img):
     """
     # Set rho and theta ranges
     W, H = img.shape
-    diag_len = int(np.ceil(np.sqrt(W * W + H * H)))
-    rhos = np.linspace(-diag_len, diag_len, diag_len * 2 + 1)
+    diag_len = int(np.ceil(np.sqrt(W * W + H * H)))  # 对角线长度
+    rhos = np.linspace(-diag_len, diag_len, diag_len * 2 + 1)  # 整个极坐标ρ的范围
     thetas = np.deg2rad(np.arange(-90.0, 90.0))
 
     # Cache some reusable values
@@ -290,11 +357,15 @@ def hough_transform(img):
     accumulator = np.zeros((2 * diag_len + 1, num_thetas), dtype=np.uint64)
     ys, xs = np.nonzero(img)
 
-    # Transform each point (x, y) in image
+    # Transform each point (x, y) in image 将图像坐标转换到极坐标下
     # Find rho corresponding to values in thetas
     # and increment the accumulator in the corresponding coordiate.
     ### YOUR CODE HERE
-    pass
+    for y, x in zip(ys, xs):
+        for i in range(num_thetas):
+            rho = round(x*cos_t[i] + y*sin_t[i])
+            rho += diag_len   # 范围变成0-2ρ
+            accumulator[rho,i] += 1
     ### END YOUR CODE
 
     return accumulator, rhos, thetas
